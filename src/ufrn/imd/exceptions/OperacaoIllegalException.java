e410110dc308c0a550d7ab17df9437b363712ef4
AroldoFe <junioraroldo37@gmail.com>
2021-04-24 15:01:42 -0300

    Conta #2 Adicionando exception para operação ilegal

---
+++ src/ufrn/imd/exceptions/OperacaoIllegalException.java
@@ -0,0 +1,13 @@
+package ufrn.imd.exceptions;
+
+public class OperacaoIllegalException extends Exception {
+	
+	public OperacaoIllegalException() {
+		super();
+	}
+	
+	public OperacaoIllegalException(String message) {
+		super(message);
+	}
+
+}
