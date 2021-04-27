30747c7efa057322969f9921031a4020a9a00751
AroldoFe <junioraroldo37@gmail.com>
2021-04-27 19:25:26 -0300

    Adicionando anotações da Operação de Débito

--- src/ufrn/imd/interfaces/OperacaoDebitar.java
+++ src/ufrn/imd/interfaces/OperacaoDebitar.java
@@ -3,6 +3,18 @@ package ufrn.imd.interfaces;
 import ufrn.imd.exceptions.OperacaoIllegalException;
 
 public interface OperacaoDebitar {
-
+	
+	//@ public model instance double saldo;
+	
+	/*@ public normal_behavior
+	@		requires valor > 0 && valor <= saldo;
+	@		assignable saldo;
+	@		ensures saldo == \old(saldo)-valor;
+	@	also
+	@		public exceptional_behavior
+	@			requires valor <= 0 && valor > saldo;
+	@			assignable \nothing;
+	@			signals_only OperacaoIllegalException;
+	@*/
 	void debitar(double valor) throws OperacaoIllegalException;
 }
